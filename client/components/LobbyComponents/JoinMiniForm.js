892e8ebde60e91d53d9c18c6f9b72332bcedf832
benjaminpwagner@gmail.com <benjaminpwagner@gmail.com>
2018-12-24 23:28:42 -0500

    force user to add deck

--- client/components/LobbyComponents/JoinMiniForm.js
+++ client/components/LobbyComponents/JoinMiniForm.js
@@ -14,6 +14,21 @@ class JoinMiniForm extends React.Component {
     this.joinButton = this.joinButton.apply(this)
     this.toggleAdd = this.toggleAdd.bind(this)
   }
+
+  forceAddDeck = () => {
+    if (this.state.isAdding === false)
+      this.setState({isAdding: true})
+  }
+
+  componentDidMount() {
+    if (!this.props.hasDecks)
+      this.forceAddDeck.apply(this)
+  }
+
+  componentDidUpdate() {
+    if (!this.props.hasDecks)
+      this.forceAddDeck.apply(this)
+  }
   
   toggleAdd = () => this.setState({isAdding: !this.state.isAdding})
 
@@ -32,10 +47,12 @@ class JoinMiniForm extends React.Component {
     return this.state.isAdding 
       ? (
         <div>
+          {this.props.hasDecks && <button onClick={this.toggleAdd}>
+            Cancel
+          </button>}
           <AddDeckForm redirect={this.toggleAdd}/>
         </div>
-      )
-      : (
+      ) : (
         <div>
           <button onClick={this.toggleAdd}>
             Add Deck
@@ -46,8 +63,15 @@ class JoinMiniForm extends React.Component {
   }
 }
 
+const mapState = state => ({
+  hasDecks: Object.keys(state.decks).reduce( 
+    (prev, curr) => state.decks[curr].format === state.user.selectedFormat 
+      ? true : prev
+    , false
+  )
+})
 const mapDispatch = { joinMini }
 
 export default withRouter(
-  connect(null, mapDispatch)(JoinMiniForm)
+  connect(mapState, mapDispatch)(JoinMiniForm)
 )
