50ee0b1996b24ea843b95dd0adb0a6c78d6b8307
benjaminpwagner@gmail.com <benjaminpwagner@gmail.com>
2019-01-28 12:10:46 -0500

    judges can countinue to tertiery rounds

--- client/components/LobbyComponents/MiniJudgePanel.js
+++ client/components/LobbyComponents/MiniJudgePanel.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
 import {getMini, startMini, nextRound} from '../../store'
 
-const MiniJudgePanel = ({match, getMini, startMini}) => {
+const MiniJudgePanel = ({match, getMini, startMini, nextRound}) => {
   const mini = getMini(match.params.miniId)
 
   const showButtons = () => {
