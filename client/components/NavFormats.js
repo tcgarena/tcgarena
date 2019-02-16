e57d93cf07746362402dba979f0487d826cecf2d
benjaminpwagner@gmail.com <benjaminpwagner@gmail.com>
2019-02-15 23:56:16 -0500

    auto select first format if you dont have a standard deck in decks menu

--- client/components/NavFormats.js
+++ client/components/NavFormats.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {selectFormat} from '../store'
+import store, {selectFormat} from '../store'
 
-const NavFormats = ({formats, selectedFormat, selectFormat}) => (
-  <div id='lobby-navbar-formats'>
-  {
-    formats.map( (format, idx) => {
-      const isSelected = format === selectedFormat
-      return (
-        <div onClick={event => selectFormat(event.target.innerHTML)} key={idx} className={`lobby-navbar-item ${isSelected ? 'lobby-navbar-selected' : ''}`}>
-          {format}
-        </div>
-      )
-    })
-  }
-  </div>
-) 
+const NavFormats = ({formats, selectedFormat, selectFormat}) => {
+  let foundSelected = false
+  const nav = formats.map( (format, idx) => {
+    const isSelected = format === selectedFormat
+    if (isSelected) foundSelected = true
+    return (
+      <div onClick={event => selectFormat(event.target.innerHTML)} key={idx} className={`lobby-navbar-item ${isSelected ? 'lobby-navbar-selected' : ''}`}>
+        {format}
+      </div>
+    )
+  })
+
+  if (!foundSelected) store.dispatch(selectFormat(formats[0]))
+
+  return (
+    <div id='lobby-navbar-formats'>
+      {nav}
+    </div>
+  ) 
+}
 
 const mapStateToProps = ({user: {selectedFormat}}) => ({selectedFormat})
 const mapDispatchToProps = { selectFormat }
