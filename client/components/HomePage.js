adafce95a50ae85beb7083a362943dade80f046b
benjaminpwagner@gmail.com <benjaminpwagner@gmail.com>
2019-02-16 01:05:23 -0500

    add beta warning to home page

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -11,6 +11,7 @@ export default () => <div id='home-page'>
   <p>Our goal is to create a friendly, yet competitive community where players can participate in ranked tournaments and single matches.</p>
   <p>Just like magic-league, TCGArena will have weekly trial tournaments, and larger free-to-enter monthly tournaments with cash prizes.</p>
   <p>We will be using cockatrice (rooster ranges server) as we develop the core features of our app, but we plan to support other platforms in the future.</p>
+  <p>We are currently in open beta with rolling updates.</p>
 
   <div id='home-page-features'>
     <h2>Current Features</h2>
