456f18c6a6dfb3866c2fc2e96b80c7b5986b2bf1
benjaminpwagner@gmail.com <benjaminpwagner@gmail.com>
2019-02-22 19:00:12 -0500

    fix delete deck styling from decksmenu

--- client/components/ConfirmAction.js
+++ client/components/ConfirmAction.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default ({text, confirm, deny, confirmText, denyText}) => {
 
   return (
-    <div>
+    <div className='row'>
       {text && <p>{'Are you sure?'}</p>}
       <button className='global-button' onClick={confirm}>
         {!!confirmText ? confirmText : 'Yes'}
