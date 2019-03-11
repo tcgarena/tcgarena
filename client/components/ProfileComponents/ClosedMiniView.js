807d1380d71dde778d5b9dd3d0c6018ab20396a1
benjaminpwagner@gmail.com <benjaminpwagner@gmail.com>
2019-03-11 18:16:00 -0400

    passable closed mini view page

--- client/components/ProfileComponents/ClosedMiniView.js
+++ client/components/ProfileComponents/ClosedMiniView.js
@@ -7,6 +7,7 @@ import msToTime from '../../utils/msToTime'
 const ClosedMiniView = ({history, match}) => {
   const [mini, setMini] = useState({})
   const [timeUntilPublic, setTime] = useState(1)
+  const [userlist, setUserlist] = useState('')
 
   const fetchMini = async () => {
     const {data: mini} = await axios.get(`/api/minis/closed/${match.params.miniUuid}`)
@@ -15,6 +16,8 @@ const ClosedMiniView = ({history, match}) => {
       const dayMs = 1000*60*60*24
       const timeLeft = dayMs - (Date.now() - new Date(mini.updatedAt).getTime())
       setTime(timeLeft)
+    } else {
+      setUserlist(mini.winner)
     }
   }
 
@@ -24,22 +27,24 @@ const ClosedMiniView = ({history, match}) => {
     if (timeUntilPublic > 1000) {
       const timer = window.setInterval(() => setTime(time => time - 1000), 1000)
       return () => window.clearInterval(timer)
-    } else if (!mini.overFor24hrs) {
+    } else if (!mini.overFor24hrs && Object.keys(mini).length) {
       // add a delay just incase the client slightly disagrees with the server
       setTimeout(() => fetchMini(), 1000)
     }
   }, [timeUntilPublic])
 
   const showMini = () => {
-    console.log(mini)
     const participantsArr = Object.keys(mini.users)
       .reduce( (arr, key) => {
         const user = mini.users[key]
         const winner = mini.winner === user.cockatriceName
         arr.push(<div 
-          className='single-mini-participants' 
+          className={`single-mini-participants pointer ${
+            userlist === user.cockatriceName ? 'highlight' : ''
+          }`}
           key={user.cockatriceName}
-          onClick={()=>history.push(`/user/${user.cockatriceName}`)}
+          onClick={() => history.push(`/user/${user.cockatriceName}`)}
+          onMouseOver={() => mini.overFor24hrs && setUserlist(user.cockatriceName)}
         >
           {user.cockatriceName} ({user.ELO}) {winner && '(winner)'}
         </div>)
@@ -48,11 +53,15 @@ const ClosedMiniView = ({history, match}) => {
 
     return <div className='closed-mini-view'>
       <div>{mini.format} {mini.type}</div>
-      <div>{(new Date(mini.createdAt).toLocaleDateString())}</div>
-      <div className='single-mini-participants-container'>
-        {participantsArr}
+      <div>{(new Date(mini.createdAt).toLocaleString())}</div>
+      {!mini.overFor24hrs && <div>{msToTime(timeUntilPublic)} until decklists go public.</div>}
+      <div className='row' style={{marginTop: 5}}>
+        <div style={{marginRight: 10}}>
+          {participantsArr}
+        </div>
+        {mini.overFor24hrs && userlist!== '' && <DeckPreview list={mini.users[userlist].decklist} />}
       </div>
-      <div>{!mini.overFor24hrs && msToTime(timeUntilPublic)}</div>
+
     </div>
   }
 
